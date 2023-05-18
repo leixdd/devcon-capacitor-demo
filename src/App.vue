558f21fd1cbe74a2131a36dc6c14780f8bb2b8fb
leixdd <lecias.ariel@gmail.com>
2023-05-18 10:40:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+<div class="bg-gray-900 h-100vh">
+
+</div>
+<section class="text-gray-400 bg-gray-900 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+      <img class="object-cover object-center rounded" alt="hero" src="https://devcon.ph/wp-content/uploads/brizy/imgs/DEVCON-Logo-White-Text-394x112x0x14x394x86x1640458595.png">
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Mobile Development
+        <br class="hidden lg:inline-block">Ionic Capacitor with Vue.js
+      </h1>
+      <p class="mb-8 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis vestibulum justo. In et nisi hendrerit, tempor purus quis, egestas nisi. Nullam cursus risus sed est bibendum facilisis. Cras sagittis odio ut tellus tincidunt lobortis. Nam maximus, purus eget imperdiet malesuada, ante felis dictum tellus, a porta nulla leo eget nulla. </p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+        <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<style>
+body {
+  --tw-bg-opacity: 1;
+    background-color: rgb(17 24 39 / var(--tw-bg-opacity));
+}
+</style>
+
